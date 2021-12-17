c37b36aeb74db6958dc0a59c686369b6966e8f2c
soosaasees <nachokaese@gmx.net>
2021-12-17 23:23:42 +0100

    bittebitte lass die scheisse jetzt mal pushen

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const port=process.env.PORT
+var express=require('express');
+var app=express();
+app.use(express.static('public'));
+app.listen(port)
+
 function loginCheck()	{
 	var goalWord="hunter2";
 	var goalMail="huehne@htw-berlin.de";
--- server.js
+++
@@ -1,6 +0,0 @@
-
-const port=process.env.PORT
-var express=require('express');
-var app=express();
-app.use(express.static('public'));
-app.listen(port)
