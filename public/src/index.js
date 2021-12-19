50c7e9d935f7359e81314ad5db10ee2680f9ce80
soosaasees <nachokaese@gmx.net>
2021-12-19 22:20:02 +0100

    vertipper ausgemerzt

--- public/src/index.js
+++ public/src/index.js
@@ -4,7 +4,7 @@ var app=express();
 app.use(express.static('public'));
 app.listen(port)
 
-konst knex = require("knex");
+const knex = require("knex");
 const config=require(".../knexfile");
 const db=knex(config.production);
 
