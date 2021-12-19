0e545d7bcfc4560b539891c5586ab4621541cd64
soosaasees <nachokaese@gmx.net>
2021-12-19 23:33:41 +0100

    lass es doch einfach mal funktionieren

--- public/src/knexfile.js
+++ public/src/knexfile.js
@@ -1,7 +1,16 @@
 module.exports = {
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      user: "uvfjvlxsebprxb",
+      password: "40c4eb4f9f70673a9461a47a0c4c1c9e3f1eada0d09a558d8450b8a5e542f682",
+      host: "ec2-34-242-89-204.eu-west-1.compute.amazonaws.com",
+      port: 5432,
+      database: "dbc6fqcv0t223t",
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     pool: {
       min: 2,
       max: 10,
