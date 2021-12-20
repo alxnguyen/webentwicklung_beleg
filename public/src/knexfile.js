40329ab9a06860597863d79776f5b9703e067431
alxnguyen <alexander.nguyen1509@gmail.com>
2021-12-20 16:34:51 +0100

    test

--- public/src/knexfile.js
+++ public/src/knexfile.js
@@ -1,17 +1,7 @@
 module.exports = {
   production: {
     client: "pg",
-    connection: {
-      user: "uvfjvlxsebprxb",
-      password: "40c4eb4f9f70673a9461a47a0c4c1c9e3f1eada0d09a558d8450b8a5e542f682",
-      host: "ec2-34-242-89-204.eu-west-1.compute.amazonaws.com",
-      port: 5432,
-      database: "dbc6fqcv0t223t",
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    },
+    connection: DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
---
+++ seeds/users.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('table_name').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('user').insert([
+        {id: 1, email: "user1@email.com", password: "password1"},
+        {id: 2, email: 'user2@email.com', password: "password2"}
+      ]);
+    });
+};
