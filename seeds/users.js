150702036b8db66942c2e39fe99c843f6598dd42
alxnguyen <alexander.nguyen1509@gmail.com>
2021-12-20 17:29:07 +0100

    udpated seed

--- seeds/users.js
+++ seeds/users.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('user').del()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('user').insert([
+      return knex('users').insert([
         {id: 1, email: "user1@email.com", password: "password1"},
         {id: 2, email: 'user2@email.com', password: "password2"},
         {id: 3, email: 'user3@email.com', password: "password3"}
