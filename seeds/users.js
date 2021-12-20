56383212929f86959a1646e25ecb2bc4d53b578d
alxnguyen <alexander.nguyen1509@gmail.com>
2021-12-20 17:08:40 +0100

    added seed

--- seeds/users.js
+++ seeds/users.js
@@ -6,7 +6,8 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('user').insert([
         {id: 1, email: "user1@email.com", password: "password1"},
-        {id: 2, email: 'user2@email.com', password: "password2"}
+        {id: 2, email: 'user2@email.com', password: "password2"},
+        {id: 3, email: 'user3@email.com', password: "password3"}
       ]);
     });
 };
